09527e444f802f600329737546cfb6473175e3cf
nshcat <nshcat@users.noreply.github.com>
2019-11-24 20:49:58 +0100

    commit§

--- bootstrap.js
+++ bootstrap.js
@@ -159,7 +159,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/lsystems_web_bg.wasm":"5b4e4fed4bf0421a4c94"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/lsystems_web_bg.wasm":"f588812b71b4adf22acb"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
